8a3bd6821410613d7fe85902b6b161875ca69a3e
Xabibax <ale23@hotmail.fr>
2026-05-01 22:04:23 +0200

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,23 +4,33 @@ void main() {
         final var scanner = new Scanner(System.in);
         final var line = scanner.nextLine();
 
-        final var command = Command.getCommandFrom(line);
-        final var parameters = Arrays.stream(line.split(" ")).skip(1).toList();
+
+        final var splitLine = line.split(" ");
+
+        final var command = Command.getCommandFrom(splitLine[0]);
+        final var parameters = Arrays.stream(splitLine).skip(1).toList();
 
 
         switch (command) {
-            case ECHO -> echo(parameters);
+            case NOT_FOUND -> printCommandNotFound(line);
             case EXIT -> {
                 return;
             }
-            case NOT_FOUND -> printCommandNotFound(line);
             case BLANK -> {}
+            case ECHO -> echo(parameters);
+            case TYPE -> type(parameters);
         }
     }
 }
 
 private static void echo(List<String> parameters) {
     String message = String.join(" ", parameters);
+    System.out.println(message);
+}
+
+private static void type(List<String> parameters) {
+    final var command = Command.getCommandFrom(parameters.getFirst());
+    final var message = command.type().orElse(parameters.getFirst() + ": not found");
     System.out.println(message);
 }
 
@@ -33,23 +43,31 @@ private static void printPrompt() {
 }
 
 enum Command {
-    BLANK,
-    ECHO,
-    EXIT,
     NOT_FOUND,
+    BLANK,
+    EXIT,
+    ECHO,
+    TYPE,
     ;
 
     public static Command getCommandFrom(String line) {
-        final var command = line.split(" ")[0];
+        final var command = line.trim().split(" ")[0].toUpperCase();
         if(command.isBlank()) {
             return BLANK;
         }
 
-        return switch (command) {
-            case "exit" -> EXIT;
-            case "echo" -> ECHO;
-            default -> NOT_FOUND;
-        };
+        try {
+            return Command.valueOf(command);
+        } catch (IllegalArgumentException e) {
+            return NOT_FOUND;
+        }
 
+    }
+
+    public Optional<String> type() {
+        return switch (this) {
+            case BLANK, NOT_FOUND -> Optional.empty();
+            default -> Optional.of(name().toLowerCase() + " is a shell builtin");
+        };
     }
 }
