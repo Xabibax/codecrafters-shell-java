1386e755c5e81d22b325cf50aa40926220fb40b6
Xabibax <ale23@hotmail.fr>
2026-05-01 21:18:33 +0200

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-         System.out.print("$ ");
+        while (true) {
+            System.out.print("$ ");
+            final var scanner = new Scanner(System.in);
+            final var command = scanner.nextLine();
+
+            switch (command) {
+                default -> System.out.printf("%s: command not found%n", command);
+            }
+        }
     }
 }
