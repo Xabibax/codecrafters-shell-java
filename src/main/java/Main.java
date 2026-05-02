9903afd5999684e47e9fabc29a4cf46391607951
Xabibax <ale23@hotmail.fr>
2026-05-02 12:40:31 +0200

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,11 @@
+import jdk.jshell.spi.ExecutionControl;
+
+public static final String PATH = "PATH";
+
 void main() {
     while (true) {
         printPrompt();
-        final var scanner = new Scanner(System.in);
-        final var line = scanner.nextLine();
+        final var line = IO.readln();
 
 
         final var splitLine = line.split(" ");
@@ -10,32 +13,67 @@ void main() {
         final var command = Command.getCommandFrom(splitLine[0]);
         final var parameters = Arrays.stream(splitLine).skip(1).toList();
 
+        if (!command.builtIn) {
+            final var res = handleExecutable(splitLine);
+            continue;
+        }
 
         switch (command) {
             case NOT_FOUND -> printCommandNotFound(line);
-            case EXIT -> {
-                return;
+            case EXIT -> exit(parameters);
+            case BLANK -> {
             }
-            case BLANK -> {}
             case ECHO -> echo(parameters);
             case TYPE -> type(parameters);
         }
     }
 }
 
+private int handleExecutable(String[] splitLine) {
+    final var paths = getPaths();
+
+    final var executable = splitLine[0];
+    final var parameters = Arrays.stream(splitLine).skip(1).collect(Collectors.joining(" "));
+
+    throw new RuntimeException("handleExecutable not yet implemented");
+}
+
+private static List<String> getPaths() {
+    return Arrays.stream(System.getenv(PATH).split(":")).toList();
+}
+
 private static void echo(List<String> parameters) {
     String message = String.join(" ", parameters);
-    System.out.println(message);
+    IO.println(message);
+}
+
+private static void exit(List<String> parameters) {
+    System.exit(0);
 }
 
 private static void type(List<String> parameters) {
-    final var command = Command.getCommandFrom(parameters.getFirst());
-    final var message = command.type().orElse(parameters.getFirst() + ": not found");
-    System.out.println(message);
+    if (Objects.requireNonNull(parameters, "parameters shouldn't be null").isEmpty()) {
+        IO.println(Command.BLANK.type(""));
+        return;
+    }
+    var command = Command.getCommandFrom(parameters.getFirst());
+
+    final var message = command.type(parameters.getFirst());
+    IO.println(message);
+}
+
+private static Optional<File> handleExecutableSearch(String command) {
+    final var paths = getPaths();
+
+    return paths.stream()
+            .map(path -> Path.of(path, command).toFile())
+            .filter(File::isFile)
+            .filter(File::canExecute)
+            .findAny();
 }
 
 private static void printCommandNotFound(String command) {
-    System.out.printf("%s: command not found%n", command);
+    IO.print("%s: command not found%n".formatted(command));
 }
 
 private static void printPrompt() {
@@ -43,31 +81,43 @@ private static void printPrompt() {
 }
 
 enum Command {
-    NOT_FOUND,
-    BLANK,
-    EXIT,
-    ECHO,
-    TYPE,
+    NOT_FOUND, BLANK, EXIT, ECHO, TYPE, EXECUTABLE(false),
     ;
 
-    public static Command getCommandFrom(String line) {
-        final var command = line.trim().split(" ")[0].toUpperCase();
-        if(command.isBlank()) {
+    final boolean builtIn;
+
+    Command() {
+        this(true);
+    }
+
+    Command(boolean isBuiltIn) {
+        this.builtIn = isBuiltIn;
+    }
+
+    public static Command getCommandFrom(String command) {
+        if (command.isBlank()) {
             return BLANK;
         }
 
         try {
-            return Command.valueOf(command);
+            return Command.valueOf(command.toUpperCase());
         } catch (IllegalArgumentException e) {
-            return NOT_FOUND;
+            return handleExecutableSearch(command).isPresent() ? EXECUTABLE : NOT_FOUND;
         }
 
     }
 
-    public Optional<String> type() {
+    public String type(String command) {
         return switch (this) {
-            case BLANK, NOT_FOUND -> Optional.empty();
-            default -> Optional.of(name().toLowerCase() + " is a shell builtin");
+            case BLANK, NOT_FOUND -> Objects.requireNonNull(command) + ": not found";
+            case EXECUTABLE -> typeExecutable(command);
+            default -> name().toLowerCase() + " is a shell builtin";
         };
+    }
+
+    private String typeExecutable(String command) {
+        return handleExecutableSearch(command)
+                .map(f -> command + " is " + f.getAbsolutePath())
+                .orElse(Command.NOT_FOUND.type(command));
     }
 }
